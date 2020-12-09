8edd8a7b0b1e5a2e7ea0269fba9ae6b8eb4a6a4d
Lauriacunia <lauriacunia@gmail.com>
2020-12-09 18:03:56 -0300

    Comienzo con los metodos GET. Buscar todas las artesanas, buscar por id, y buscar por nombre y telefono

--- index.js
+++ index.js
@@ -9,6 +9,58 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
+app.use((req,res,next) => {
+    console.log('*******************************************')
+    console.log( 'URL:    ', req.url )
+    console.log( 'MÉTODO: ', req.method )
+    console.log( 'QUERY:  ', req.query )
+    console.log( 'BODY:   ', req.body )
+    console.log('*******************************************')
+    next()
+})
+
+/***◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆*
+ *                       GET
+ **◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆* */
+
+// get all --> devuelve todas las artesanas
+app.get('/artesanas', (req, res) =>{
+    res.send({ message: 'todas las artesanas'})
+})
+
+// get by Id --> devuelve una artesana, busca por id
+
+
+app.get('/artesanas/:id?', (req,res) => {
+  res.send(`<b>Buscando artesana por id</b>`) 
+})
+
+
+// get by name or phone--> devuelve una artesana, busca por nombre o celu
+app.get('/artesanas/:name/:phone?', (req,res) => {
+  res.send(`<b>Buscando artesana por nombre o telefono</b>`) 
+})
+
+/* ----------------------------------------------------- */
+
+app.get('*', (req,res) => {
+    let url = req.url
+    let method = req.method
+    res.status(500).send(`<b style="color:red;">La ruta ${method} ${url} no está implementada</b>`)
+})
+
+ /***◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆*
+ *                       POST
+ **◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆* */
+
+/* ----------------------------------------------------- */
+app.post('*', (req,res) => {
+    let url = req.url
+    let method = req.method
+    res.status(500).send(`<b style="color:crimson;">La ruta ${method} ${url} no está implementada</b>`)
+})
+
+
 app.listen(port, () => {
     console.log(`Escuchando en http://localhost:${port}`)
 })
