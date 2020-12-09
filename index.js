a334ff87fb7fa8bbbb80a03505d169b1471ccc13
Lauriacunia <lauriacunia@gmail.com>
2020-12-09 18:16:02 -0300

    Avanzo con los metodos POST, PUT y DELETE. Funcionalidades: registrar a una artesana, modificar sus datos por id y borrar su registro por id.

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ app.get('/artesanas', (req, res) =>{
 })
 
 // get by Id --> devuelve una artesana, busca por id
-
-
 app.get('/artesanas/:id?', (req,res) => {
   res.send(`<b>Buscando artesana por id</b>`) 
 })
@@ -53,12 +51,42 @@ app.get('*', (req,res) => {
  *                       POST
  **◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆* */
 
+//Registro de una artesana
+app.put('/artesanas/:name/:phone', (req,res) => {
+    let body = req.body
+    res.send(`<b style="color:crimson;">Se registro con exito a la artesana: ${body}</b>`)
+})
+
 /* ----------------------------------------------------- */
 app.post('*', (req,res) => {
     let url = req.url
     let method = req.method
     res.status(500).send(`<b style="color:crimson;">La ruta ${method} ${url} no está implementada</b>`)
 })
+
+
+/***◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆*
+ *                       PUT
+ **◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆* */
+
+// Modificacion de un registro de artesana por id
+
+app.put('/artesanas/:id', (req,res) => {
+    let id = req.params.id
+    res.send(`<b style="color:green;">La artesana con el id: ${id} fue actualizada en la db</b>`)
+})
+
+ /***◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆*
+ *                       DELETE
+ **◇*◆*◇*◆*◇*◆*◇*◆*◇*◆**◇*◆*◇*◆*◇*◆*◇*◆*◇*◆* */
+
+// Delete by Id --> Borra una artesana por Id
+
+app.delete('/artesanas/:id', (req,res) => {
+    let id = req.params.indice
+    res.send(`<b style="color:crimson;">La artesana con el id: ${id} fue borrada de la db</b>`)
+})
+
 
 
 app.listen(port, () => {
